b524e205306939695c57d482172398838d06cd9f
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2026-02-04 21:07:34 +0700

    Added 202602042107

--- 02_3762_Variable/Solution/src/main.rs
+++ 02_3762_Variable/Solution/src/main.rs
@@ -13,4 +13,17 @@ fn main() {
     println!("The Z is {Z}");
     // z = 300;
     // println!("The x is {z}");
+
+    let (i1, i2, i3, i4, i5):(i8,i16,i32,i64,i128) = (-5,-5,-5,-5,-5);
+    let (u1, u2, u3, u4, u5):(u8,u16,u32,u64,u128) = (5,5,5,5,5);
+    let (f1,f2):(f32,f64) = (5.23,7.56);
+    let (b1,b2):(bool,bool)=(true,false);
+    let c1:char = 'z';
+
+    let t1:(i32,f32,bool)=(5,5.868,true);
+    println!("{} {} {}",t1.0, t1.1, t1.2);
+    let a1:[i32;3] = [1,2,3];
+    println!("{} {} {}",a1[0],a1[1],a1[2]);
+    let s1:&str ="Uaychai Naja";
+    println!("{s1}");
 }
