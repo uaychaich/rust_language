4379b6c4e1afd2bb6b0a88ea30b6b795a57ef884
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2026-02-04 14:50:17 +0700

    Added 202602041450

---
+++ 01_b79e_Intro/Solution/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // Hi, This is a comment.
+    println!("Hello, world!");
+}
---
+++ 02_3762_Variable/Solution/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let x = 20;
+    println!("The x is {x}");
+    // x = 30;
+    // println!("The x is {x}");
+
+    let mut y = 200;
+    println!("The y is {y}");
+    y = 300;
+    println!("The y is {y}");
+
+    const Z:i32 = 2000;
+    println!("The Z is {Z}");
+    // z = 300;
+    // println!("The x is {z}");
+}
