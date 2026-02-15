e02a8d1a1c41caf4cac0c46ac1670ae1b8c42460
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2026-02-15 21:58:54 +0700

    Added 202602152159

---
+++ 04_6611_Decision/Solution/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let a1:i32 = 5;
+
+    if a1 <5 {
+        print!("a1 is less than 5: {}\n",a1)
+    }else if a1==5{
+        print!("a1 is equal 5: {}\n",a1)
+    }else{
+        print!("a1 is more than 5: {}\n",a1)
+    }
+
+    /////////////////////////////////////////////////////////
+    
+    let a2 = if a1==5 {"Five"}else{"Else"};
+    print!("a2={}\n",a2);
+    
+    /////////////////////////////////////////////////////////
+    
+    let a3:i32 =5;
+    
+}
