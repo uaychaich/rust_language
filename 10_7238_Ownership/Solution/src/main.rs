4f9103cd9c74510e1f95af4ff6cb88b56c043608
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2026-02-18 22:38:06 +0700

    Added 202602182238

---
+++ 10_7238_Ownership/Solution/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // let a11 = 5; //Variable that keeps a data in stack
+    // let a12 = &a11; //Variable that keeps a address that ref to other variable
+    // let a13 = String::from("Uaychai"); //Variable that keep a address that ref to a data in heap
+    // println!("a11={} | a12={:p} | a13={:p}",a11,a12,a13.as_ptr());
+    // println!("//////////////////////////////////////////////////");
+    ////////////////////////////////////////////////////////////////////////////////////////
+    // let a21 = 7;
+    // {
+    //     let a22 =5;
+    //     println!("a21={} | a22={}",a21, a22);
+    // }
+    // println!("a21={} | a22={}",a21, a22);
+    ////////////////////////////////////////////////////////////////////////////////////////
+    
+}
