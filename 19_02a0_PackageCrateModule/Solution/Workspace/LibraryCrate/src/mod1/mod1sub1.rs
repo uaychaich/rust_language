2598c8c78a0e0684ec9dbe687ff049caf9295bf7
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2026-02-25 09:27:01 +0700

    Added 202602250927

---
+++ 19_02a0_PackageCrateModule/Solution/Workspace/LibraryCrate/src/mod1/mod1sub1.rs
@@ -0,0 +1,3 @@
+pub fn mod1sub1filename_function() -> String {
+        "Hello from mod1sub1! filename".to_string()
+}
---
+++ 19_02a0_PackageCrateModule/Solution/Workspace/LibraryCrate/src/mod2.rs
@@ -0,0 +1,3 @@
+pub fn mod2filename_function() -> String {
+        "Hello from mod2! filename".to_string()
+}
